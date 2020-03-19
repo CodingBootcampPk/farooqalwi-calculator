a366384c6cd6ac17d4c409416570585c8d9ca752
Farooq Alwi <farooqalwi313@gmail.com>
2020-03-19 23:44:35 +0500

    History class created

---
+++ Simple Calculator/History.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    class History
+    {
+        private List<History> CalHistory;
+
+        public History()
+        {
+            CalHistory = new List<History>();
+        }
+    }
+}
